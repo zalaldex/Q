03902463ad659c47fdeb44c6fe756025b12c8001
zalaldex <ptnium@gmail.com>
2026-07-27 08:28:22 +0600

    Add bot/__init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,24 @@
+"""
+bot package initializer
+"""
+
+__all__ = [
+    "constants",
+    "logger",
+    "database",
+    "models",
+    "keyboards",
+    "utils",
+    "monospace",
+    "chunker",
+    "media",
+    "backup",
+    "restore",
+    "statistics",
+    "settings",
+    "sender",
+    "handlers",
+    "services",
+]
+
+__version__ = "0.1.0"
